371906069c274032e0624ab4e600635b29ca2b1d
mkarmark <mikarmar@microsoft.com>
2023-05-15 19:57:30 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ];
+  },
+};

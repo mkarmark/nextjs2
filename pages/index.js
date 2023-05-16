b9bff39b983388737afab9e1c12557888c8e73f6
mkarmark <mikarmar@microsoft.com>
2023-05-15 18:53:38 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,17 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://dummyjson.com/products/1`);
+  const data = await res.json();
+ 
+  // Pass data to the page via props
+  return { props: { data } };
+}
+
+export default function Home({ data }) {
   return (
     <>
       <Head>
